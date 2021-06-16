2f40dccbe8e8294c248ab8fd958cb6c57eba0f36
Andri Reveli <reveliandri@gmail.com>
2021-06-16 19:59:25 +0200

    fixed elisp list

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -58,7 +58,7 @@
 
 (setq
  doom-theme 'doom-dark+
- projectile-project-search-path '("~/tmp/", "~/projects/"))
+ projectile-project-search-path '("~/tmp/" "~/projects/"))
 
 ;; keybindings for switching tabs
 (evil-define-key 'normal centaur-tabs-mode-map (kbd "C-<right>") 'centaur-tabs-forward         ; default Doom binding is 'g t'
