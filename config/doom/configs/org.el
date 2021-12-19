97a8dc071fb76b9ad00b9efc7e27b58116d3eb10
Andri Reveli <reveliandri@gmail.com>
2021-12-19 19:10:52 +0100

    Customize org agenda

--- config/doom/configs/org.el
+++ config/doom/configs/org.el
@@ -3,16 +3,39 @@
 ;; show when org heading is folded
 (setq org-ellipsis " ▾")
 
-(setq org-superstar-headline-bullets-list '(8278 8280 8281))
-(setq org-superstar-cycle-headline-bullets nil)
+;; headings
+(setq org-superstar-headline-bullets-list '(8278 8280 8281)
+      org-superstar-cycle-headline-bullets nil)
 
 ;; display italic, bold, etc. inline words without markers
 (setq org-hide-emphasis-markers t)
 
 ;; line height for each org heading
-(custom-set-faces
-  '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
-  '(org-level-2 ((t (:inherit outline-2 :height 1.1))))
-  '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
-  '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
-  '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
+(custom-set-faces '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
+                  '(org-level-2 ((t (:inherit outline-2 :height 1.1))))
+                  '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
+                  '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+                  '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
+
+(after! org (setq! org-todo-keywords '((sequence "TODO(t)"
+                                                "INPROGRESS(i)"
+                                                "|"
+                                                "DONE(d)"
+                                                "CANCELLED(c)"))
+                   org-todo-keyword-faces '(("TODO"
+                                             :foreground "7c7c75"
+                                             :weight normal
+                                             :underline t)
+                                            ("INPROGRESS"
+                                             :foreground "0098dd"
+                                             :weight normal
+                                             :underline t)
+                                            ("DONE"
+                                             :foregrounf "50a14f"
+                                             :weight normal
+                                             :underline t)
+                                            ("CANCELLED"
+                                             :foreground "ff6480"
+                                             :weight normal
+                                             :underline t))
+                   org-log-done 'time))
