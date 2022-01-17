e5ded0936160b9eeb1c40727b8eb042c7b05a231
Andri Reveli <reveliandri@gmail.com>
2022-01-17 19:45:26 +0100

    Display emphasis markers

--- config/doom/configs/org.el
+++ config/doom/configs/org.el
@@ -7,9 +7,6 @@
 (setq org-superstar-headline-bullets-list '(8278 8280 8281)
       org-superstar-cycle-headline-bullets nil)
 
-;; display italic, bold, etc. inline words without markers
-(setq org-hide-emphasis-markers t)
-
 ;; line height for each org heading
 (custom-set-faces '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
                   '(org-level-2 ((t (:inherit outline-2 :height 1.1))))
