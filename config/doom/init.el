fbca42d9cda0f2a90fde244ac686787ac08740ed
Andri Reveli <reveliandri@gmail.com>
2022-03-10 07:58:22 +0100

    Disable syntax checking and create setup for docker

--- config/doom/init.el
+++ config/doom/init.el
@@ -76,16 +76,16 @@
        ;;vterm                  ; the best terminal emulation in Emacs
 
        :checkers
-       syntax                   ; tasing you for every semicolon you forget
-       (spell +flyspell)        ; tasing you for misspelling mispelling
-       grammar                  ; tasing grammar mistake every you make
+       ;;syntax                 ; tasing you for every semicolon you forget
+       ;;(spell +flyspell)      ; tasing you for misspelling mispelling
+       ;;grammar                ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
        ;;biblio                 ; Writes a PhD for you (citation needed)
        ;;debugger               ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       ;;docker
+       docker
        ;;editorconfig           ; let someone else argue about tabs vs spaces
        ;;ein                    ; tame Jupyter notebooks with emacs
        (eval +overlay)          ; run code, run (also, repls)
@@ -164,7 +164,7 @@
        ;;swift                  ; who asked for emoji variables?
        ;;terra                  ; Earth and Moon in alignment for performance.
        ;;web                    ; the tubes
-       ;;yaml                   ; JSON, but readable
+       yaml                     ; JSON, but readable
        ;;zig                    ; C, but simpler
 
        :email
