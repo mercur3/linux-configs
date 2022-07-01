d6c9586f4c50c6a831be9fee7b816112c94a3d42
Andri Reveli <reveliandri@gmail.com>
2022-07-01 19:03:58 +0200

    Make org headings larger

--- config/doom/configs/org.el
+++ config/doom/configs/org.el
@@ -10,9 +10,9 @@
       org-superstar-cycle-headline-bullets nil)
 
 ;; line height for each org heading
-(custom-set-faces '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
-                  '(org-level-2 ((t (:inherit outline-2 :height 1.1))))
-                  '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
+(custom-set-faces '(org-level-1 ((t (:inherit outline-1 :height 1.3))))
+                  '(org-level-2 ((t (:inherit outline-2 :height 1.2))))
+                  '(org-level-3 ((t (:inherit outline-3 :height 1.1))))
                   '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
                   '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
 
